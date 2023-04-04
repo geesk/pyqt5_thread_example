d81e320a9d57c37d365d5608a7829e2287bfee75
Jie Seok Kim <jieseok.kim@qrtkr.com>
2023-04-04 14:52:56 +0900

    fix : pyqtSignal을 사용하도록 수정

--- main.py
+++ main.py
@@ -2,20 +2,19 @@ import sys
 import time
 
 from PyQt5 import uic
-from PyQt5.QtCore import QThread
+from PyQt5.QtCore import QThread, pyqtSignal
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 form_class = uic.loadUiType("main_ui.ui")[0]
-
 class main_window(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.thread = example_thread(self)
+        self.thread = example_thread()
 
         # start()를 통해 run()을 시작
         self.thread.start()
-
+        self.thread.progress.connect(self.progressBar.setValue)
         # line edit에 있는 text를 label에 넣는다.
         self.pushButton.clicked.connect(self.set_label_text)
 
@@ -23,17 +22,18 @@ class main_window(QMainWindow, form_class):
         self.label.setText(self.lineEdit.text())
 
 class example_thread(QThread):
-    def __init__(self, env):
+    progress = pyqtSignal(int)
+    def __init__(self):
         super().__init__()
-        self.env = env
 
     def run(self):
         # 1초마다 progress bar의 value를 1씩 증가시킨다.
+        value = 0
         while True:
-            value = self.env.progressBar.value() + 1
+            value += 1
             if value > 100:
                 value = 0
-            self.env.progressBar.setValue(value)
+            self.progress.emit(value)
             time.sleep(1)
 
 if __name__ == "__main__":
